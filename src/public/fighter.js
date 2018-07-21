f61e3ec570e388d49d020a94f650e2639e442203
Volodymyr <34250821+VoBilyk@users.noreply.github.com>
2018-07-21 13:27:53 +0300

    implemented classes fighter and improvedFighter

--- src/public/fighter.js
+++ src/public/fighter.js
@@ -1,1 +1,28 @@
-// Create class Fighter
+class Fighter {
+    constructor(name, power, health) {
+        this.name = name;
+        this.power = power;
+        this.health = health;
+    }
+
+    setDamage(damage) {
+        this.health -= damage;
+        console.log('health: ${this.health}');
+    }
+
+    hit(enemy, point) {
+        let damage = point * power;
+        enemy.setDamage(damage);
+    }
+
+    knockout() {
+        return new Promise((resolve, reject) => {
+            console.log('time is over');
+            setTimeout(() => {
+                resolve();
+            }, 500);
+        });
+    }
+}
+
+export { Fighter };
--- src/public/improvedFighter.js
+++ src/public/improvedFighter.js
@@ -1,1 +1,13 @@
-// Create class ImprovedFighter
+import Fighter from './fighter.js'
+
+class ImprovedFighter extends Fighter {
+    constructor(name, power, health) {
+        super(name, power, health);
+    }
+
+    doubleHit(enemy, point) {
+        this.hit(enemy, point * 2)
+    }
+}
+
+export { ImprovedFighter };
